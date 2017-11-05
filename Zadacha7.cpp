f9231495f3c5c5d0442f2397a4318ca890b51e94
selineminova <32775976+selineminova@users.noreply.github.com>
2017-11-05 23:39:23 +0200

    Add files via upload

---
+++ Zadacha7.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number,i;
+	cin>>number;
+	for(i=number;i>=1;i--)
+	{
+		if(i%7==0)
+		{
+			cout<<i<<endl;
+		}
+	}
+
+	
+
+	
+	
+	
+}
